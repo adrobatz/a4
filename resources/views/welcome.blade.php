fb4cc519e6f6ab94334d7c6b3e1b94aa2beb7a79
Alyssa Drobatz <drobatzalyssa@gmail.com>
2017-05-03 21:06:12 -0400

    test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ASSIGNMENT 4
                 </div>
 
                 <div class="links">
