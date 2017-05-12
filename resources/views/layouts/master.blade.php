c57028b14664018665076009505d03cf9643457f
Alyssa Drobatz <drobatzalyssa@gmail.com>
2017-05-11 22:49:34 -0400

    css updates

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,35 +2,29 @@
 <html>
 
 <head>
-    <title>
-        My Wedding Website
-    </title>
+    <title>My Wedding Website</title>
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
     <link href="/css/style.css" type='text/css' rel='stylesheet'> 
-
     @stack('head')
 </head>
 
 <header>
-	<h1>Our Wedding</h1>
-<nav>
-	
-<ul>
-	<li><a href="/rsvp">For Guests</a></li>
-	<li><a href="/allrsvps">For the Bride &amp; Groom</a></li>
-</ul>
-
-</nav>
-
+    <h1>Our Wedding</h1>
+    <nav>
+        <ul>
+            <li><a href="/rsvp">For Guests</a></li>
+            <li><a href="/allrsvps">For the Bride &amp; Groom</a></li>
+        </ul>
+    </nav>
 </header>
 
 <body>
 
-
     @if(Session::get('message') != null)
-    <div class='message'>{{ Session::get('message') }}</div>
+        <div class='message'>{{ Session::get('message') }}</div>
     @endif
+    
     <section>
         @yield('content')
     </section>
--- resources/views/rsvp/index.blade.php
+++ resources/views/rsvp/index.blade.php
@@ -5,14 +5,12 @@
 	<h1>List of RSVPs</h1>
 
 	@foreach($rsvps as $rsvp)
-		<p><a href='/rsvp/tag/{{$rsvp->id}}'>Organize me</a></p>
-		<p><a href='/rsvp/delete/{{$rsvp->id}}'>Delete me</a></p>
-		<h2>{{$rsvp['name']}}</h2>
-		<p>{{$rsvp['email']}}</p>
-		<p>{{$rsvp['address']}}</p>
-		<p>{{$rsvp['attending']}}</p>
-		<p>{{$rsvp['guest']}}</p>
-		<p>{{$rsvp['meal']}}</p>
+		<h2>Guest Name: {{$rsvp['name']}}</h2>
+		<p>Guest Email: {{$rsvp['email']}}</p>
+		<p>Guest Address: {{$rsvp['address']}}</p>
+		<p>Guest RSVP: {{$rsvp['attending']}}</p>
+		<p>Additional Guest: {{$rsvp['guest']}}</p>
+		<p>Guest Meal: {{$rsvp['meal']}}</p>
 
 	@foreach($tagsForCheckboxes as $id => $name)
 		<input
@@ -24,7 +22,8 @@
 		{{ $name }} <br>
 
 	@endforeach
-
+		<div ><a class="editButtons" href='/rsvp/tag/{{$rsvp->id}}'>Organize RSVP</a></div>
+		<div class="editButtons"><a href='/rsvp/delete/{{$rsvp->id}}'>Delete RSVP</a></div>
 	@endforeach
 
 	@endsection
